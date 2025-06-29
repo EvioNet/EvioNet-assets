f25f7c035c9fd5830d249defdcf24665d9df1c14
EvioNet <admin@evionet.com>
2025-06-29 13:32:26 +0800

    Add files via upload

---
+++ assets/mode-toggle-BAwNESTv-1751174980531.js
@@ -0,0 +1,11 @@
+import{d as r,j as e}from"./index-zp2nMl9D-1751174980531.js";import{B as n}from"./button-Cqc4v3GU-1751174980531.js";import{c as t}from"./createLucideIcon-Ru6o5lSX-1751174980531.js";/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z",key:"a7tn18"}]],m=t("moon",c);/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=[["circle",{cx:"12",cy:"12",r:"4",key:"4exip2"}],["path",{d:"M12 2v2",key:"tus03m"}],["path",{d:"M12 20v2",key:"1lh1kg"}],["path",{d:"m4.93 4.93 1.41 1.41",key:"149t6j"}],["path",{d:"m17.66 17.66 1.41 1.41",key:"ptbguv"}],["path",{d:"M2 12h2",key:"1t8f8n"}],["path",{d:"M20 12h2",key:"1q8mjw"}],["path",{d:"m6.34 17.66-1.41 1.41",key:"1m8zz5"}],["path",{d:"m19.07 4.93-1.41 1.41",key:"1shlcs"}]],l=t("sun",h);function p(){const{theme:a,setTheme:s}=r(),o=()=>{s(a==="dark"?"light":"dark")};return e.jsxs(n,{variant:"ghost",size:"icon",onClick:o,children:[e.jsx(l,{className:"h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"}),e.jsx(m,{className:"absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"}),e.jsx("span",{className:"sr-only",children:"Toggle theme"})]})}export{p as M};
